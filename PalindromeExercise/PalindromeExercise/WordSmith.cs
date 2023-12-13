4390acd079d61799610d1c78d5a4b735176018ed
Conscience Osakue <consciencenosa@gmail.com>
2023-12-13 14:18:44 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word == null || word.Length == 0)
+            {
+                return false;
+            }
+            
+            string wordlowered = word.ToLower();
+            string backwards = "";
+
+            for (int i = wordlowered.Length - 1; i >= 0; i--)
+            {
+                backwards += wordlowered[i];
+            }
+            return backwards == wordlowered;
+
+            // if (backwards == wordlowered)
+            //{
+              //  return true;
+            //}
+           // else { return false; }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,33 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("civic", true)]
+        [InlineData("Mom", true)]
+        [InlineData("level", true)]
+        [InlineData("Radar", true)]
+        [InlineData("frank", false)]
+        [InlineData("", false)]
+        [InlineData("song", false)]
+        [InlineData(null, false)]
 
+        public void Test1(string word, bool expected)
+        {
+            //Arrange
+            var ws = new WordSmith();
+
+            //Act
+            var actual = ws.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
